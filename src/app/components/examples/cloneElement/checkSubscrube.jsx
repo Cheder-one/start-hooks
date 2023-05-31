6800e5c8a04b007a774f0c4b6d5b37d4a7c7b1ec
Cheder-one <cherbadenis1@gmail.com>
2023-05-31 22:27:21 +0300

    u

--- src/app/components/examples/cloneElement/checkSubscrube.jsx
+++ src/app/components/examples/cloneElement/checkSubscrube.jsx
@@ -1,23 +1,45 @@
 /* eslint-disable */
-import React from "react";
+import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
+
+const someText = faker.lorem.paragraphs(5);
+
+const getTwoParagraphs = (text) => {
+   const arrParagraphs = text.split(/\n/);
+   return arrParagraphs.length > 2
+      ? [
+           arrParagraphs.slice(0, 2),
+           "...",
+           "Для продолжения оформите подписку"
+        ].join("\n")
+      : text;
+};
 
 const Text = ({ text }) => {
    return <p>{text}</p>;
 };
 
-const getTwoParagraphs = (text) => {
-   console.log(text);
-   const arrParagraphs = text.split(/\n/);
-   console.log(arrParagraphs);
-};
-
 const CheckSubscribe = () => {
-   const someText = faker.lorem.paragraphs(5);
+   const [isSubscribe, setIsSubscribe] = useState(false);
    const myText = <Text text={someText} />;
 
-   getTwoParagraphs(myText.props.text);
-   return <div>CheckSubscribe</div>;
+   const availableText = React.cloneElement(myText, {
+      text: isSubscribe
+         ? myText.props.text
+         : getTwoParagraphs(myText.props.text)
+   });
+
+   return (
+      <div>
+         {availableText}
+         <button
+            className="btn btn-primary"
+            onClick={() => setIsSubscribe((s) => !s)}
+         >
+            Подписаться
+         </button>
+      </div>
+   );
 };
 
 export default CheckSubscribe;
