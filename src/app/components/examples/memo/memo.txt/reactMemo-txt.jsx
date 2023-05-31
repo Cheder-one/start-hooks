0eab127b13e89ec1360d1c7a7820a22e8e78bfe4
Cheder-one <cherbadenis1@gmail.com>
2023-05-31 20:16:17 +0300

    u

--- src/app/components/examples/memo/memo.txt/reactMemo-txt.jsx
+++ src/app/components/examples/memo/memo.txt/reactMemo-txt.jsx
@@ -1,36 +1,45 @@
 /* eslint-disable */
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 const Parent = () => {
    const [count, setCount] = useState(0);
 
+   const user = useMemo(
+      () => ({
+         name: "Sarah Sullivan",
+         phone: "1-976-631-1449",
+         email: "fringilla.purus.mauris@protonmail.com",
+         address: "2660 Frangible Av."
+      }),
+      []
+   );
+
+   const handleCount = useCallback(() => {
+      setCount((c) => c + 1);
+   }, []);
+
    return (
       <div>
          <h2>Count: {count}</h2>
-         <button
-            className="btn btn-primary"
-            onClick={() => setCount((c) => c + 1)}
-         >
-            +
-         </button>
+
          <div>
-            <Children />
+            <Children user={user} onIncrement={handleCount} />
          </div>
       </div>
    );
 };
 
-const Children = React.memo(() => {
-   const renderCount = useRef(0);
-
+const Children = React.memo(({ user, onIncrement }) => {
    useEffect(() => {
-      renderCount.current += 1;
-      console.log("render");
+      console.log("Children rerender");
    });
 
    return (
       <div className="mt-2 p-1 card d-inline-block">
-         {JSON.stringify(renderCount)}
+         {user.name}
+         <button className="btn btn-primary ms-2 btn-sm" onClick={onIncrement}>
+            +
+         </button>
       </div>
    );
 });
