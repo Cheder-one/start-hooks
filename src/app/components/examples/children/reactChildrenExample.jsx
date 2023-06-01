19c3ff79c1e1eb4d337bebddb63c82579ddb051f
Cheder-one <cherbadenis1@gmail.com>
2023-06-01 14:32:49 +0300

    u

--- src/app/components/examples/children/reactChildrenExample.jsx
+++ src/app/components/examples/children/reactChildrenExample.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import TextField from "../../common/form/textField";
+
+const FormComponent = ({ children }) => {
+   const [data, setData] = useState({});
+
+   const handleChange = (target) => {
+      setData((prev) => ({ ...prev, [target.name]: target.value }));
+   };
+
+   return React.Children.map(children, (child) => {
+      return React.cloneElement(child, { onChange: handleChange });
+   });
+};
+
+FormComponent.propTypes = {
+   children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+   ])
+};
+
 const ReactChildrenExample = () => {
    return (
       <CardWrapper>
          <SmallTitle>Clone form and add props</SmallTitle>
+         <hr />
+         <FormComponent>
+            <TextField name="email" label="Email" />
+            <TextField name="password" label="Password" type="password" />
+         </FormComponent>
       </CardWrapper>
    );
 };
